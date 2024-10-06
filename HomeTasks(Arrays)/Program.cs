f035ea3742ddad1cb3fd1b0136867e0702818168
RavanBayramov1 <ravanvb-ab108@code.edu.az>
2024-10-06 21:26:29 +0400

    HomeTasks added

---
+++ HomeTasks(Arrays)/Program.cs
@@ -0,0 +1,39 @@
+﻿//Task1
+//int ans = 1;
+//int num = 5, pow = 4;
+//for (int i = 1; i <= pow; i++)
+//{
+//    ans = ans * num;
+//}
+//Console.WriteLine(ans);
+
+
+//Task2
+//int num = 15462;
+//int digit = 0;
+//while (num > 0)
+//{
+//    num = num / 10;
+//    digit++;
+//}
+//Console.WriteLine(digit);
+
+
+//Task3
+//int num = 0;
+//int[,] numbers =
+//{
+//    {1, 2, 22},
+//    {4, 19, 5}
+//};
+//for (int i = 0; i < numbers.GetLength(0); i++)
+//{
+//    for (int j = 0; j < numbers.GetLength(1); j++)
+//    {
+//        if (numbers[i,j]/10 == 0)
+//        {
+//            num++;
+//        }
+//    }
+//}
+//Console.WriteLine(num + " reqem var");
