39839098f390003be09b2241a0557b294f6a8d2b
RavanBayramov1 <ravanvb-ab108@code.edu.az>
2024-10-09 02:02:33 +0400

    Arrays' compares added

---
+++ HomeTask(Arrays3)/Program.cs
@@ -0,0 +1,41 @@
+﻿//Task1
+
+//int[] arr1 = { 1, 5, 8, 11, 17 };
+//int[] arr2 = { 1, 2, 6, 11, 22 };
+
+//for (int i = 0; i < arr1.Length; i++)
+//{
+//        if (arr1[i] != arr2[i])
+//        {
+//            Console.Write(arr1[i] + " ");
+//        }
+//}
+
+//Task2
+
+//int sumOdd = 0;
+//int sumEven = 0;
+//int[] arr = { 1, 15, 8, 11, 17,};
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (i%2 == 0)
+//    {
+//        sumEven += arr[i];
+//    }
+//    else
+//    {
+//        sumOdd += arr[i];
+//    }
+//}
+//if (sumOdd > sumEven)
+//{
+//    Console.WriteLine(" Tek indeksdeki ededlerin cemi boyukdur");
+//}
+//else if (sumEven > sumOdd)
+//{
+//    Console.WriteLine(" Cut indeksdeki ededlerin cemi boyukdur");
+//}
+//else
+//{
+//    Console.WriteLine(" Tek indeksdeki ededlerin cemi cut indeksdeki ededlerin cemine beraberdir");
+//}
