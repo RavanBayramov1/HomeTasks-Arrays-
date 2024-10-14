11e02ffc0e1c9ac64987d92cb64293a55d2ca46d
RavanBayramov1 <ravanvb-ab108@code.edu.az>
2024-10-15 01:24:47 +0400

    ProductTask added

---
+++ BrendPrice Task/BrendPrice Task/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrendPrice_Task
+{
+     public class Product
+    {
+        public string Name;
+        public string BrandName;
+        public double Price;
+        public Product(string Name,string BrandName, double Price)
+        {
+            this.Name = Name;
+            this.BrandName = BrandName;
+            this.Price = Price;
+        }
+    }
+}
---
+++ BrendPrice Task/BrendPrice Task/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace BrendPrice_Task
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Product Yag = new Product("Kere yag", "Anchor", 17.55);
+            Product Duyu = new Product("Basmati", "Sultan", 8.79);
+            Product Sud = new Product("2.5%li", "AzerSud", 1.65);
+            Product Cay = new Product("Earl Grey", "AzerCay", 6.2);
+            Product Salfetka = new Product("Tualet Kagizi", "Aura", 24.9);
+            Product[] Products = {Yag, Cay, Sud, Duyu, Salfetka};
+            ProductFilter(Products,30,0);
+        }
+        static void ProductFilter(Product[] Products, double MaxPrice, double MinPrice)
+        {
+            bool have = false;
+            for (int i = 0; i < Products.Length; i++)
+            {
+                if (Products[i].Price < MaxPrice && Products[i].Price > MinPrice)
+                {
+                    have = true;
+                    Console.WriteLine(Products[i].BrandName + " " + Products[i].Price);
+                }
+
+            }
+            if (!have)
+            {
+                Console.WriteLine("Mehsul tapilmadi!");
+            }
+        }
+    }
+}
